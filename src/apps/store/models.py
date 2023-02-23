1976929e7f19ce362e2a92e2c8cfd280f667d48e
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-02-23 11:52:30 +0700

    adding membership choices step 1

--- src/apps/store/models.py
+++ src/apps/store/models.py
@@ -2,11 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-    name = models.CharField(max_length=225)
+    MEMBERSHIP_CHOICES = [
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold'),
+    ]
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(null=True)
+    membership = models.CharField(max_length=255, choices=MEMBERSHIP_CHOICES, default= 'B')
+    
 
 class Product(models.Model):
     title = models.CharField(max_length=225)
-    price = models.IntegerField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2) # 9999,99
+    inventory = models.IntegerField
+    last_update = models.DateTimeField()
 
 
     
