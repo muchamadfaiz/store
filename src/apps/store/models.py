97f6cbce9c64d24e5d90f56a126bd65766c2079f
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-02-23 12:48:00 +0700

    adding membership choices step 2

--- src/apps/store/models.py
+++ src/apps/store/models.py
@@ -2,17 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    
     MEMBERSHIP_CHOICES = [
         ('B', 'Bronze'),
         ('S', 'Silver'),
         ('G', 'Gold'),
     ]
+
     first_name = models.CharField(max_length=225)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateTimeField(null=True)
-    membership = models.CharField(max_length=255, choices=MEMBERSHIP_CHOICES, default= 'B')
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default= MEMBERSHIP_BRONZE)
     
 
 class Product(models.Model):
@@ -21,6 +27,21 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2) # 9999,99
     inventory = models.IntegerField
     last_update = models.DateTimeField()
+
+PAYMENT_STATUS_PENDING = 'P'
+PAYMENT_STATUS_COMPLETE = 'C'
+PAYMENT_STATUS_FAILED = 'F'
+
+PAYMENT_STATUS_CHOICES = [
+    ('P', 'Pending'),
+    ('C', 'Complete'),
+    ('F', 'Failed'),
+]
+
+class Order (models.Model):
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+
 
 
     
